70838ef1a18f756bf38d2e6a3045663a981110c7
Samuel Lopes <samuelvitorprofissional@gmail.com>
2022-01-12 14:11:32 -0300

    fim

---
+++ index.php
@@ -0,0 +1,11 @@
+<?
+$message=md5(rand(0,9999));
+$files = scandir('./messages');
+$num_files = count($files)-2;
+$fileName="$message.txt";
+$file= fopen("./messages/$fileName","x");
+fwrite($file,$message);
+fclose($file);
+
+
+?>
